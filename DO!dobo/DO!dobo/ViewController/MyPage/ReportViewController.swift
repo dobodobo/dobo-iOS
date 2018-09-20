20c366d42fb8c0c172edf1541081717f85a67d58
kyeahen <bright19961@naver.com>
2018-09-20 17:18:49 +0900

    건의사항 키보드 세팅

--- DO!dobo/DO!dobo/ViewController/MyPage/ReportViewController.swift
+++ DO!dobo/DO!dobo/ViewController/MyPage/ReportViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ReportViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var c: NSLayoutConstraint!
+    var constraintInitVal : CGFloat = 0
+        var check = true
+        var keyboardDismissGesture: UITapGestureRecognizer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setBackBtn()
+        setKeyboardSetting()
 
         // Do any additional setup after loading the view.
     }
@@ -24,14 +31,60 @@ class ReportViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension ReportViewController {
+    
+    func setKeyboardSetting() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: true)
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+
+            if check {
+                constraintInitVal = c.constant
+                c.constant += keyboardSize.height
+                
+                self.view.layoutIfNeeded()
+                check = false
+            }
+            
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        adjustKeyboardDismissGesture(isKeyboardVisible: false)
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+
+            c.constant = constraintInitVal
+
+            self.view.layoutIfNeeded()
+            check = true
+        }
+    }
+    
+    func adjustKeyboardDismissGesture(isKeyboardVisible: Bool) {
+        if isKeyboardVisible {
+            if keyboardDismissGesture == nil {
+                keyboardDismissGesture = UITapGestureRecognizer(target: self, action: #selector(tapBackground))
+                view.addGestureRecognizer(keyboardDismissGesture!)
+            }
+        } else {
+            if keyboardDismissGesture != nil {
+                view.removeGestureRecognizer(keyboardDismissGesture!)
+                keyboardDismissGesture = nil
+            }
+        }
+    }
+    
+    @objc func tapBackground() {
+        self.view.endEditing(true)
+    }
+    ////////
+}
+
